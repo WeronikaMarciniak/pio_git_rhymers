e087135b5464092dba9847822237cadd095e9ab5
Weronika Marciniak <weronika_marciniak@interia.pl>
2021-01-30 20:23:32 +0100

    refactory

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,6 +6,14 @@ public class DefaultCountingOutRhymer {
 
 	public int total = -1;
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;
